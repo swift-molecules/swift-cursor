6811c55904e6c4bf864283075197080140ce91c0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:06:41 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-cursor-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -26,11 +26,26 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-tagged-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ordinal-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-cardinal-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-index-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ownership-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-tagged-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ordinal-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-cardinal-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-index-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ownership-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         // MARK: - Namespace
@@ -41,7 +56,10 @@ let package = Package(
                 .product(name: "Tagged Primitives", package: "swift-tagged-primitives"),
                 .product(name: "Ordinal Primitives", package: "swift-ordinal-primitives"),
                 .product(name: "Cardinal Primitives", package: "swift-cardinal-primitives"),
-                .product(name: "Ownership Borrow Primitives", package: "swift-ownership-primitives"),
+                .product(
+                    name: "Ownership Borrow Primitives",
+                    package: "swift-ownership-primitives"
+                ),
             ]
         ),
 
@@ -50,7 +68,7 @@ let package = Package(
         .target(
             name: "Cursor Primitives",
             dependencies: [
-                "Cursor Primitive",
+                "Cursor Primitive"
             ]
         ),
 
@@ -70,7 +88,7 @@ let package = Package(
         .testTarget(
             name: "Cursor Primitives Tests",
             dependencies: [
-                "Cursor Primitives",
+                "Cursor Primitives"
             ]
         ),
     ],
